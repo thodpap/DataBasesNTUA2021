46aa56f92fda2ac22c29f46fa13f71f9d73ea3fd
thodpap <paparrigopoulosthodoris@gmail.com>
2021-06-20 12:33:09 +0300

    added services

--- frontend/src/app/services/service-list.service.ts
+++ frontend/src/app/services/service-list.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class ServiceListService {
     services = [
-        ' ', 
-        'Pool',
+        ' ',  
         'Gym', 
+        'Saouna',
+        'Meeting Room'
     ]
 }
